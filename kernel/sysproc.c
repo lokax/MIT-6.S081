3dafaa21fccc6aaeb5804d04ac1ec66e37889aae
Wstarry <m632656684@gmail.com>
2021-01-30 11:14:00 +0800

    not

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -47,7 +47,13 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
- 	myproc()->sz += n;
+ 	// myproc()->sz += n;
+  if(n < 0) {
+    if(myproc()->sz + n < 0) {
+      return -1;
+    }
+  }
+  myproc()->sz += n;
 	 // if(growproc(n) < 0)
   	//   return -1;
   return addr;
--- kernel/vm.c
+++ kernel/vm.c
@@ -317,9 +317,13 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
-      panic("uvmcopy: pte should exist");
+      // panic("uvmcopy: pte should exist");
+      // my code here
+      continue;
     if((*pte & PTE_V) == 0)
-      panic("uvmcopy: page not present");
+      // panic("uvmcopy: page not present");
+      // my code here
+      continue;
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
     if((mem = kalloc()) == 0)
